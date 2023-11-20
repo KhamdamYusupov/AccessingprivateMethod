a6916d65fdbfcd41a7c169866ffa6ec32ac1d43d
Khamdam <140491314+KhamdamYusupov@users.noreply.github.com>
2023-11-20 12:46:51 +0500

    Updated ABC

--- src/Abc.java
+++ src/Abc.java
@@ -2,5 +2,6 @@ public class Abc {
 
     private void display() {
         System.out.println("Accessing the private method from different class!!!");
+        System.out.println("Weel done")
     }
 }
